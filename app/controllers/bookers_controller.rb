955cb40bdef22971fc801c578bba9fdb1bd5e615
hapi190601 <gc1206ro@gmail.com>
2021-02-12 03:38:17 +0000

    [0212-1240]

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -22,8 +22,23 @@ class BookersController < ApplicationController
   end
 
   def edit
+    @booker = Booker.find(params[:id])
   end
 
+  def update
+    booker = Booker.find(params[:id])
+    booker.update(booker_params)
+    redirect_to show_path(booker.id)
+  end
+
+  def destroy
+    booker = Booker.find(params[:id])
+    booker.destroy
+    redirect_to "/bookers/index"
+  end
+
+
+  # ストロングパラメータ
   private
   def booker_params
     params.permit(:title, :body)
